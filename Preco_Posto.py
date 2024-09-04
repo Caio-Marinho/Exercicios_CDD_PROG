fdc32b045b1aa35da697941cf5295202d888cb4a
Caio-Marinho <kaigabriel12@gmail.com>
2024-09-04 14:43:39 -0300

    Exercicio 05-09-2024

---
+++ Preco_Posto.py
@@ -0,0 +1,23 @@
+"""Escreva um algoritmo que leia o número de litros vendidos e o tipo de combustível
+(codificado da seguinte forma: E-etanol, G-gasolina),
+calcule e imprima o valor a ser pago pelo cliente,
+sabendo-se que o preço do litro da gasolina é R$ 5,80 e o preço do litro do etanol é R$ 4,90."""
+litro = float(input("Informe quantos litros foi colocado em seu veiculo: "))
+tipoCombustivel = input("Informe qual tipo de combustivel foi colocado. \n"
+                        "Digite...\n(E) para Etanol e (G) para Gasolina: ")
+
+if (tipoCombustivel != "E" and tipoCombustivel != "e") and (tipoCombustivel != "G" and tipoCombustivel != "g"):
+    print("Esse tipo de combustivel não existe!!")
+else:
+    if tipoCombustivel == "E" or tipoCombustivel == "e":
+        tipoCombustivel = 'Etanol'
+        valorCombustivel = 4.90
+        valorTotal = litro * valorCombustivel
+        print(f"O combustivel escolhido foi o {tipoCombustivel} tendo colocado {litro} Litro(s) totalizando "
+              f"um valor total de R$ {valorTotal:.2f}")
+    else:
+        tipoCombustivel = 'Gasolina'
+        valorCombustivel = 5.80
+        valorTotal = litro * valorCombustivel
+        print(f"O combustivel escolhido foi o {tipoCombustivel} tendo colocado {litro} Litro(s) totalizando "
+              f"um valor total de R$ {valorTotal:.2f}")
