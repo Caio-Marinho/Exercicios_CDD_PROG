e30e49eacbce5fa4361dec74b4b5a7a403b459dd
Caio-Marinho <kaigabriel12@gmail.com>
2024-09-04 16:41:10 -0300

    Exercicio 05-09-2024

--- Preco_Posto.py
+++ Preco_Posto.py
@@ -2,12 +2,15 @@
 (codificado da seguinte forma: E-etanol, G-gasolina),
 calcule e imprima o valor a ser pago pelo cliente,
 sabendo-se que o preço do litro da gasolina é R$ 5,80 e o preço do litro do etanol é R$ 4,90."""
+import time
+i = time.time()
 litro = float(input("Informe quantos litros foi colocado em seu veiculo: "))
 combustivel = input("Informe qual tipo de combustivel foi colocado. \n"
                     "Digite...\n(E) para Etanol e (G) para Gasolina: ")
 
 if (combustivel != "E" and combustivel != "e") and (combustivel != "G" and combustivel != "g"):
     print("Esse tipo de combustivel não existe!!")
+else:
     if combustivel == "E" or combustivel == "e":
         tipoCombustivel = 'Etanol'
         valorCombustivel = 4.90
@@ -20,3 +23,5 @@ if (combustivel != "E" and combustivel != "e") and (combustivel != "G" and combu
         valorTotal = litro * valorCombustivel
         print(f"O combustivel escolhido foi o {tipoCombustivel} tendo colocado {litro} Litro(s) totalizando "
               f"um valor total de R$ {valorTotal:.2f}")
+f = time.time()
+print(f-i)
