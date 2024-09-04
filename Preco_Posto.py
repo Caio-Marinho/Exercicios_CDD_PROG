4437ab125c925102b74d808d42860ff7bbe7b30c
Caio-Marinho <kaigabriel12@gmail.com>
2024-09-04 16:13:12 -0300

    Exercicio 05-09-2024(Mudança da variavel global tipoCombustivel para somente combustivel).

--- Preco_Posto.py
+++ Preco_Posto.py
@@ -3,13 +3,13 @@
 calcule e imprima o valor a ser pago pelo cliente,
 sabendo-se que o preço do litro da gasolina é R$ 5,80 e o preço do litro do etanol é R$ 4,90."""
 litro = float(input("Informe quantos litros foi colocado em seu veiculo: "))
-Combustivel = input("Informe qual tipo de combustivel foi colocado. \n"
+combustivel = input("Informe qual tipo de combustivel foi colocado. \n"
                     "Digite...\n(E) para Etanol e (G) para Gasolina: ")
 
-if (Combustivel != "E" and Combustivel != "e") and (Combustivel != "G" and Combustivel != "g"):
+if (combustivel != "E" and combustivel != "e") and (combustivel != "G" and combustivel != "g"):
     print("Esse tipo de combustivel não existe!!")
 else:
-    if Combustivel == "E" or Combustivel == "e":
+    if combustivel == "E" or combustivel == "e":
         tipoCombustivel = 'Etanol'
         valorCombustivel = 4.90
         valorTotal = litro * valorCombustivel
